2b336634eb9c59ddf32ff05247edc4e844120bc5
MD Moon Babu <m.mmoon1527@gmail.com>
2026-01-23 20:17:32 +0600

    Add Animation to stars and lasers

--- Scene/level.gd
+++ Scene/level.gd
@@ -3,7 +3,21 @@ extends Node2D
 var meteor_scene : PackedScene =load("res://Scene/meteor.tscn")
 var laser_scene :PackedScene =load("res://Scene/laser.tscn")
 func _ready():
+	#Lasers
 	$Timer.start()
+	# Stars
+	var size:=get_viewport().get_visible_rect().size
+	var rng=RandomNumberGenerator.new()
+	for star in $Stars.get_children():
+		#position
+		var random_x=rng.randi_range(0,int(size.x))
+		var random_y=rng.randi_range(0,int(size.y))
+		star.position=Vector2(random_x,random_y)
+		#Scale
+		var random_scale=rng.randi_range(1,2)
+		star.scale =Vector2(random_scale,random_scale)
+		#Speed
+		star.speed_scale =rng.randf_range(0.6,1.4)
 
 func _on_timer_timeout() -> void:
 	#create instance
