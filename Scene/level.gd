167a342fe1c6fc5c8450b91c23d3a869f2f710df
MD Moon Babu <m.mmoon1527@gmail.com>
2026-01-23 16:26:38 +0600

    Add lasers with Custom signals with cooldown

--- Scene/level.gd
+++ Scene/level.gd
@@ -1,8 +1,20 @@
 extends Node2D
+#load scene
 var meteor_scene : PackedScene =load("res://Scene/meteor.tscn")
+var laser_scene :PackedScene =load("res://Scene/laser.tscn")
 func _ready():
 	$Timer.start()
 
 func _on_timer_timeout() -> void:
+	#create instance
 	var meteor =meteor_scene.instantiate()
+	#attch the node to the scene tree
 	$Meteors.add_child(meteor)
+
+
+func _on_palyer_laser(pos) -> void:
+	#create instance
+	var laser=laser_scene.instantiate()
+	#attch the node to the scene tree
+	$Laser.add_child(laser)
+	laser.position=pos
--- Scene/palyer.gd
+++ Scene/palyer.gd
@@ -1,8 +1,27 @@
 extends CharacterBody2D
+@onready var laser_cooldown = $LaserCooldown
+var can_shoot = true
 @export var speed :=500
+
+signal laser(pos)
+
+func _ready() -> void:
+	position=Vector2(1280/2,680)  
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	var direction=Input.get_vector("left","right","up","down")
 	velocity =direction*speed
 	move_and_slide()
+	
+	
+	
+	#shoot input
+	if Input.is_action_just_pressed("Shoot") and can_shoot:
+		laser.emit($LaserStartingPosition.global_position)
+		can_shoot=false
+		laser_cooldown.start()
+
+
+func _on_laser_cooldown_timeout() -> void:
+		can_shoot=true
